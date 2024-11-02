e8509cd08fc4c24126916dbb1e27654adcb33403
Лещев Петр Михайлович <pmleshchev@beeline.ru>
2024-11-02 11:57:29 +0300

    first commit

---
+++ src/test/java/org/example/demoopentelemetrypet/DemoopentelemetryPetApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.demoopentelemetrypet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoopentelemetryPetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
